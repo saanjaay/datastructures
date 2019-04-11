2fe7d0f16bf342ad298c287238df4c07bdf53351
saanjaay <sanjayselvaraj470@gmail.com>
2019-04-11 20:00:19 +0530

    insert before bugg not rectfied

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -24,13 +24,13 @@ class list
 		{
 			head = t;
 			tail = t;
-			t=NULL;
+			t->next=NULL;
 		}
 		else
 		{
 			tail->next = t;
 			tail = t;
-			tail->next =NULL;
+			tail->next =NULL;  
 		}
 	}
 	void display()
@@ -44,22 +44,107 @@ class list
 		} 
 		cout<<'\n';
 	}
+	void insert_after(int a,int val)
+	{
+		if(head != NULL)
+		{
+		node t = new nod;
+		t->data = val;
+		node pre = new nod;
+		node aft = new nod;
+		pre = aft = head;
+		while(pre->data != a)
+	{
+			pre = pre->next;
+			aft = pre;
+	}
+	    aft = pre->next;
+		pre->next = t;
+		t->next = aft;
+	    }
+	    else
+	    cout<<"Empty List";
+	}
+	void insert_before(int a, int val)
+	{
+		int f=0;
+		if(head!= NULL)
+		{
+			node t = new nod;
+			t->data = val;
+			node pre = new nod;
+			node aft = new nod;
+			aft = head;
+			while(aft->data != a)
+			{
+				f=1;
+				pre = aft;
+				aft = aft->next;
+			}
+			if(f=0)
+			{
+				t->next = head;
+				head = t;
+			}
+			else
+			{
+			pre = aft;
+			aft = aft->next;
+			pre->next = t;
+			t->next = aft;
+			}
+		}
+		else
+	    cout<<"Empty List";
+		
+	}
+	
 };
 int main()
 {
 	list a;
-	int b,c;
-	while (true)
+	int b,c,d,e,f;
+/*	while (true)
 	{
-		cout<<"\nEnter the number to be inserted:";
-		cin>>b;
-		a.insert(b);
-		cout<<"\nPress 1 to continue and 0 to exit : \n";
-		cin>>c;
-		if(c == 0)
-		break;
+		cout<<"1.Insert a number"<<'\n'<<"2.Insert after"<<'\n'<<"3.Insert before"<<'\n'<<"4.Delete after"<<'\n'<<"5.Delete Before"<<'\n'<<"6.Display\n";
+		cin>>d;
+		switch(d)
+		{
+			case 1 : 
+			cout<<"\nEnter the number to be inserted:";
+			cin>>b;
+			a.insert(b);
+			break;
+			case 2 :
+			cout<<"Enter number";
+			cin>>e;
+			cout<<"after";
+			cin>>d;
+			a.insert_after(d,e);
+			break;
+			case 6 :
+			a.display();
+			break;
+		}
 	}
-	a.display();
+		*/
+		
+		a.insert(1);
+		a.insert(2);
+		a.insert(3);
+		a.insert(4);
+		a.insert(6);
+		a.display();
+		a.insert_after(4,5);
+		a.display();
+		a.insert_before(1,0);
+		a.display();
+	
+//	cout<<"\nPress 1 to continue and 0 to exit : \n";
+	//	cin>>c;
+	//	if(c == 0)
+	//	break;
+
 	return 0;
 	
 	
