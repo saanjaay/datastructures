76c560f5a5837adbd2cd0a9692cd551e83286ce2
saanjaay <sanjayselvaraj470@gmail.com>
2019-04-11 14:57:35 +0530

    Error rectifired - insert || display code entered

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,40 +1,65 @@
 #include<iostream>
 using namespace std;
-struct node
+struct nod
 {
 int data;
-node *next;	
+nod *next;	
 };
+typedef struct nod *node;
 class list 
 {
 	private:
-		node *head,*tail;
+		node head,tail;
 	public:
 		list()
 		{
 			head = NULL;
 			tail = NULL;
 		}
-	void insert(int data)
+	void insert(int d)
 	{
-		node *temp;
-		temp->data;
+		node t = new nod;
+		t->data = d;
 		if(head == NULL)
 		{
-			head = temp;
-			tail = temp;
+			head = t;
+			tail = t;
+			t=NULL;
 		}
 		else
 		{
-			tail->next = temp;
-			tail = temp;
+			tail->next = t;
+			tail = t;
+			tail->next =NULL;
 		}
+	}
+	void display()
+	{
+		node t = new nod;
+		t = head;
+		while(t != NULL)
+		{
+			cout<<t->data<<'\t';
+			t = t->next;
+		} 
+		cout<<'\n';
 	}
 };
 int main()
 {
 	list a;
-	
+	int b,c;
+	while (true)
+	{
+		cout<<"\nEnter the number to be inserted:";
+		cin>>b;
+		a.insert(b);
+		cout<<"\nPress 1 to continue and 0 to exit : \n";
+		cin>>c;
+		if(c == 0)
+		break;
+	}
+	a.display();
 	return 0;
 	
 	
