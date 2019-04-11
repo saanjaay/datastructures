205cfc57cde401d5fa93f29b3be034839baef99e
saanjaay <sanjayselvaraj470@gmail.com>
2019-04-11 14:07:40 +0530

    Inc insert

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,12 +14,28 @@ class list
 		{
 			head = NULL;
 			tail = NULL;
-	
-		}		
+		}
+	void insert(int data)
+	{
+		node *temp;
+		temp->data;
+		if(head == NULL)
+		{
+			head = temp;
+			tail = temp;
+		}
+		else
+		{
+			tail->next = temp;
+			tail = temp;
+		}
+	}
 };
 int main()
 {
 	list a;
+	
 	return 0;
+	
 	
 }
